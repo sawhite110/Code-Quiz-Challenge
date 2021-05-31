36e296c0154865a7df7ea4662f69295a39ba13e4
SAW2Codes <stevenson.whiteii@gmail.com>
2021-05-30 22:47:40 -0400

    Updated the HTML and JS files.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Code-Quiz-Challenge
+
+---
+
+## Overview
+
+* This app will be a timed coding quiz with multiple-choice questions.
+
+* This app will dynamically run in the browser using HTML, CSS, & JavaScript.
+
+* Given the user is asked a series of questions, given 4 choices, and at the end, given the opportunity to enter in their initials and given their socre.
+
+* The last page will be a list of the users and their score.
+
+## Screen Shots
+
+* This is the first page that will render in the browser.
+
+
+
+* This is an example of 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,19 @@
-var highScores = document.querySelector("#scores");
+var scoresLink = document.querySelector("#scores");
 var timerText = document.querySelector(".timer-text");
 var timerEl = document.querySelector(".timer-count");
 var startButton = document.querySelector(".start-button");
 var intro = document.querySelector("#intro");
 var questions = document.querySelector("#questions");
+var done = document.querySelector("#done");
+var highScores = document.querySelector("#highScores");
+var highScoreList = document.querySelector("#highScoreList");
+var submit = document.querySelector("#submit");
+var clearButton = document.querySelector("#clear");
+var backButton = document.querySelector("#back");
 var choiceA = document.querySelector("#a");
 var choiceB = document.querySelector("#b");
 var choiceC = document.querySelector("#c");
 var choiceD = document.querySelector("#d");
-
 var correctAnswer = 0;
 var wrongAnswer = 0;
 var timer;
@@ -16,7 +21,13 @@ var timerCount;
 var questionIndex = 0;
 var secondsLeft = 75;
 var minusTime = -10;
-
+var message = document.querySelector("#message");
+var message2 = document.querySelector("#message2");
+var finalScore = document.querySelector("#finalScore");
+var message3 = document.querySelector("#message3");
+var initials = document.querySelector("#initials");
+var timerInterval;
+var highScoresArray = JSON.parse(localStorage.getItem("highScores"))||[];
 
 //Setting up an array of objects
 var questionArray = [
@@ -62,56 +73,64 @@ var questionArray = [
  }
 ];
 
+//Timer for the game
 function setTime() {
   // Sets interval in variable
-  var timerInterval = setInterval(function() {
+    timerInterval = setInterval(function() {
     secondsLeft--;
     timerEl.textContent = "Time: " + secondsLeft;
-
     if(secondsLeft === 0) {
       // Stops execution of action at set interval
       clearInterval(timerInterval);
+      finalScore.textContent = 0;
       // Calls function to create and append the score and user input
-      highScores();
+      questions1.style.display = "none";
+      done.style.display = "block";
+      finalScore.style.display = "block";
     }
-
   }, 1000);
 }
 
+//This will cycle through the question array
 function updateQues() {
-  var questionEl = document.getElementById("question1");
-  console.log(questionArray[questionIndex].question);
-  questionEl.textContent = questionArray[questionIndex].question;
-    choiceA = document.getElementById("a");
-  choiceA.textContent = questionArray[questionIndex].a;
-    choiceB = document.getElementById("b");
-  choiceB.textContent = questionArray[questionIndex].b;
-    choiceC = document.getElementById("c");
-  choiceC.textContent = questionArray[questionIndex].c;
-    choiceD = document.getElementById("d");
-  choiceD.textContent = questionArray[questionIndex].d;
+  if(questionIndex < questionArray.length) {
+    questions.innerHTML = questionArray[questionIndex].question
+    choiceA.innerHTML = questionArray[questionIndex].a
+    choiceB.innerHTML = questionArray[questionIndex].b
+    choiceC.innerHTML = questionArray[questionIndex].c
+    choiceD.innerHTML = questionArray[questionIndex].d
+  } else {
+    clearInterval(timerInterval);
+    finalScore.textContect = secondsLeft;
+    questions1.style.display = "none";
+    done.style.display = "block";
+    console.log(highScoresArray); //(highScoresArray)
+    console.log(highScoresArray[0].score);
+  }
+
 }
 
 //Capturing the intro container and setting it to display none while intro to first question and answers.
 startButton.addEventListener("click", function() {
   intro.style.display = "none";
   questions1.style.display = "block";  
-
   updateQues();
   setTime();
   // updateQues2();
-
 });
 
+//Capturing the buton clicks for the answers  
 choiceA.addEventListener("click", function() {
   console.log(choiceA.innerHTML);
   if (choiceA.innerHTML === questionArray[questionIndex].correct) {
-    questionArray++;
+    alert("Correct!");
+    questionIndex++;
     updateQues();
   } else {
+    alert("Wrong!");
     secondsLeft -= 10; //subtract 10 seconds from time
     //go to next question    
-    questionArray++;  
+    questionIndex++;  
     updateQues();   
   }
 });
@@ -119,44 +138,96 @@ choiceA.addEventListener("click", function() {
 choiceB.addEventListener("click", function() {
   console.log(choiceB.innerHTML);
   if (choiceB.innerHTML === questionArray[questionIndex].correct) {
-    questionArray++;
+    alert("Correct!");
+    questionIndex++;
     updateQues();
   } else {
+    alert("Wrong!");
     secondsLeft -= 10; //subtract 10 seconds from time
     //go to next question    
-    questionArray++;    
+    questionIndex++;    
     updateQues(); 
   }
-})
+});
 
 choiceC.addEventListener("click", function() {
   console.log(choiceC.innerHTML);
   if (choiceC.innerHTML === questionArray[questionIndex].correct) {
-    questionArray++;
+    alert("Correct!");
+    questionIndex++;
     updateQues();
   } else {
+    alert("Wrong!");
     secondsLeft -= 10; //subtract 10 seconds from time
     //go to next question    
-    questionArray++;    
+    questionIndex++;    
      updateQues();
   }
-})
+});
 
 choiceD.addEventListener("click", function() {
   console.log(choiceD.innerHTML);
   if (choiceD.innerHTML === questionArray[questionIndex].correct) {
-    questionArray++;
+    alert("Correct!");
+    questionIndex++;
     updateQues();
   } else {
+    alert("Wrong!");
     secondsLeft -= 10; //subtract 10 seconds from time
     //go to next question    
-    questionArray++;   
+    questionIndex++;   
     updateQues();  
   }
-})
+});
+
+// Event Listner to display the score
+submit.addEventListener("click", function() {
+  var initialsValue = initials.value;
+  var score = secondsLeft;
+  highScoresArray.push({"initials":initialsValue, "score":score});
+  localStorage.setItem("highScores", JSON.stringify(highScoresArray));
+  done.style.display = "none";
+  highScoreList.innerHTML = " ";
+  for (var i = 0; i < highScoresArray.length; i++ ) {
+    var li = document.createElement("li");
+    li.innerText = highScoresArray[i].initials + ": " + highScoresArray[i].score;
+    highScoreList.appendChild(li);
+  }
+  highScores.style.display = "block";
+});
+
+// High scores button
+var allHighScore = document.querySelectorAll("#highScoreList");
+for (var index = 0; index <allHighScore.length; index++) {
+  allHighScore[index].addEventListener("click", function() {
+  this.idList.toggle("active");
+  });
+};
+
+//View high scores link
+scoresLink.addEventListener("click", function() {
+  intro.style.display = "none";
+  highScores.style.display = "block";
+  questions1.style.display = "none";
+  done.style.display = "none";
+  secondsLeft = 0;
+  timerEl = 0;
+  timerCount = 0;
+  clearInterval(timerInterval);
+  var retrieveScoresArray = localStorage.getItem("highScores");
+  var scoresData = JSON.parse(retrieveScoresArray);
+  console.log(highScoresArray);
+  console.log(scoresData.length);
+  highScores.append(scoresData);
+});
 
 
+// Clear high scores
+clearButton.addEventListener("click", function() {
+  localStorage.clear();
+});
 
-// function highScores() {
-
-// }
+// Back button to go to the home page
+backButton.addEventListener("click", function() {
+  window.location.reload();
+});
